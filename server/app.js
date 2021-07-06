4673130c1ce0f652c5267917e28aa16b3ba74e7c
Kevin Li <jshiansongli@gmail.com>
2021-07-05 20:37:34 -0400

    countdown

--- server/app.js
+++ server/app.js
@@ -23,6 +23,49 @@ const Game = require("./models/GameInstance");
 //server connection
 mongoose.connect(key.key,{useNewUrlParser:true,useUnifiedTopology: true},()=>{console.log("MongoDB Connected");});
 
+const startGameClock = async (gameID)=>{
+    let game = await Game.findById(gameID);
+    game.startTime = new Date().getTime();
+    game = await game.save();
+    let time = 10;
+    let timerID = setInterval(function gameIntervalFunc(){
+        if(time>=0){
+            const formattedTime = calculateTime(time);
+            io.to(gameID).emit('timer',{countDown:formattedTime,msg: "Time Remaining"});
+            time--;
+        }else{
+            (async ()=>{
+                let endTime = new Date().getTime();
+                let {startTime} = game;
+                game.isOver = true;
+                game.players.forEach((players,index)=>{
+                    if(player.WPM ===-1){
+                        game.players[index].WPM = calculateWPM(endTime,startTime,player);
+                    }
+                });
+                game = await game.save();
+                io.to(gameID).emit('updateGame',game);
+                clearInterval(timerID); 
+            })();
+        }
+        return gameIntervalFunc;
+    },1000);
+}
+
+const calculateTime = (time) => {
+	let minutes = Math.floor(time / 60);
+	let seconds = time % 60;
+	return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+};
+
+const calculateWPM = (endTime, startTime, player) => {
+	let numOfWords = player.currentWordIndex;
+	const timeInSeconds = (endTime - startTime) / 1000;
+	const timeInMinutes = timeInSeconds / 60;
+	const WPM = Math.floor(numOfWords / timeInMinutes);
+	return WPM;
+};
+
 io.on('connect',(socket)=>{
     console.log("socket.io connected");
     socket.on('create-game',async (username)=>{
@@ -69,7 +112,7 @@ io.on('connect',(socket)=>{
     });
 
     socket.on('timer',async({gameID,playerID})=>{
-        let countDown = 10;
+        let countDown = 5;
         let game = await Game.findById(gameID);
         let player = game.players.id(playerID);
         if(player.isPartyLeader){
@@ -81,7 +124,7 @@ io.on('connect',(socket)=>{
                     game.isOpen = false;
                     game = await game.save();
                     io.to(gameID).emit('updateGame',game);
-                    //startGameClock(gameID);
+                    startGameClock(gameID);
                     clearInterval(timerID);
                 }
             },1000);
